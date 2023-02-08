e229f20236d737bb4a7ffacfac4d29d2f2b2a365
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-02-08 17:37:40 -0400

    refactorizacion

--- src/components/container/component-list.tsx
+++ src/components/container/component-list.tsx
@@ -1,29 +1,36 @@
 //components
 import FilePreview from "../pure/file-preview";
-import FolderPreview from "../pure/folder-preview";
 
 //models
 import { TypeArchive } from "../../models/type-archive.enum";
+import { iconsPaths } from "../../utils/icons";
 
 type Props = {
 	type: string;
 	title: string;
-	textPreview?: string;
+	textPreview?: string | null;
 	createDate: string;
+	pathFile: string;
 };
-const ComponentList = ({ type, title, textPreview, createDate }: Props) => {
+const ComponentList = ({
+	type,
+	title,
+	textPreview,
+	createDate,
+	pathFile,
+}: Props) => {
 	let component: any;
-	if (type === TypeArchive.FILE) {
-		component = (
-			<FilePreview
-				title={title}
-				textPreview={textPreview || ""}
-				createDate={createDate}
-			/>
-		);
-	} else {
-		component = <FolderPreview title={title} createDate={createDate} />;
-	}
+	let paths =
+		type === TypeArchive.FILE ? iconsPaths.fileMusic : iconsPaths.folderMusic;
+	component = (
+		<FilePreview
+			title={title}
+			textPreview={textPreview || null}
+			createDate={createDate}
+			paths={paths}
+			pathFile={pathFile}
+		/>
+	);
 	return component;
 };
 
