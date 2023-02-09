676cf37e1614c3581e86c19cc70838ca0fd350d6
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-02-09 11:33:41 -0400

    componente loading

---
+++ src/components/pure/loading.tsx
@@ -0,0 +1,11 @@
+import "../../styles/style-loading.css";
+
+const Loading = () => {
+	return (
+		<div className="loader-container">
+			<div className="loader"></div>
+		</div>
+	);
+};
+
+export default Loading;
