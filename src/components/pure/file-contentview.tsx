2486be5d70cab50040fd3ed8883c08cc40c4a02e
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-02-14 15:17:21 -0400

    diseño: FileContentView

--- src/components/pure/file-contentview.tsx
+++ src/components/pure/file-contentview.tsx
@@ -4,30 +4,48 @@ import { arrayStatesToArrayPoints } from "../metronome/utils/utils";
 
 //styles
 import "../../styles/style-files.css";
+import "../../styles/style-file-music.css";
 import BackBtn from "./back-btn";
+import { useState } from "react";
 
 /**
  * TODO: HACER LOS CAMPOS EDITABLES
  * TODO: AGREGAR BOTON PARA GUARDAR CAMBIOS
- * TODO: AGREGAR EDITOR DE TEXTO 
+ * TODO: AGREGAR EDITOR DE TEXTO
  */
 
 type Props = {
 	props: File;
 };
 const FileContentView = ({ props }: Props) => {
-	console.log(props._music);
+	const [estructura, setEstructura] = useState(`${props._music._estructure}`);
 	return (
-    <div className="page" style={{
-      gridTemplateRows:"20% 1fr"
-    }}>
-			<header className="header-app header-files">
-				<h1>{props._music._title}</h1>
-				<p>Fecha: {props._creationDate}</p>
-        <p>Autor: {props._music._autor}</p>
-        <BackBtn />
+		<div className="page-file-music">
+			<header className="header-app page-file-music-header">
+				<div className="container-title">
+					<div className="back-button">
+						<BackBtn />
+					</div>
+
+					<h1>{props._music._title}</h1>
+					<div className="save-button">
+						<BackBtn />
+					</div>
+				</div>
+
+				<div className="page-file-music-info">
+					<div className="page-file-music-date">
+						<p>Fecha: {props._creationDate}</p>
+					</div>
+					<div className="page-file-music-autor">
+						<p>Autor: {props._music._autor}</p>
+					</div>
+					<div className="page-file-music-album">
+						<p>Album: {props._music._album}</p>
+					</div>
+				</div>
 			</header>
-			<main className="main-app main-files-user">
+			<main className="page-file-music-main">
 				<Metronome
 					compassInit={props._music._metronome.compass}
 					customMetronome={{
@@ -38,6 +56,16 @@ const FileContentView = ({ props }: Props) => {
 						),
 					}}
 				/>
+				<section className="container-structure">
+					<textarea
+						className="textarea-structure"
+						id="structure"
+						value={estructura}
+						onChange={(event) => {
+							setEstructura(event.target.value);
+						}}
+					/>
+				</section>
 			</main>
 		</div>
 	);
